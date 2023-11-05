2e01593d25ffc5e778865f7262226708b846cd69
rferdev <rferreira.dev@hotmail.com>
2023-11-04 22:48:33 -0500

    test 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,8 @@ import { VuelosComponent } from './pages/vuelos/vuelos.component';
 import { ReservasComponent } from './pages/reservas/reservas.component';
 import { HomeComponent } from './pages/home/home.component';
 
+// test 2
+
 const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: HomeComponent },
