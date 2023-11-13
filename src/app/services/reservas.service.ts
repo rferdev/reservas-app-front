57166ba6fb4742c32a2ce7d8141c80eab6248bfe
rferdev <rferreira.dev@hotmail.com>
2023-11-13 13:43:42 -0500

    fix environment file call

--- src/app/services/reservas.service.ts
+++ src/app/services/reservas.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-import { environment } from 'src/environments/environment.development';
+import { environment } from 'src/environments/environment';
 
 import { Reserva } from '../interfaces/reserva.interface';
 
